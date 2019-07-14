5af7c4218e4548207371db3d7a5cf5d9cdc89645
recursionwu <recursionwu@163.com>
2019-07-14 19:22:45 +0800

    Maintain

--- config/dir.mk
+++ config/dir.mk
@@ -1,3 +1,4 @@
 export host-tools-src-dir=$(rootdir)/host-tools/src
-export packages-dir=$(rootdir)/packages
 export host-tools-outputs-dir=$(rootdir)/host-tools-outputs
+
+export packages-dir=$(rootdir)/packages
