5a3d3cfb1ba24cf76f2304ed7523f85cc12bc4d2
recursionwu <recursionwu@163.com>
2019-07-17 11:22:33 +0800

    Maintain | format code.

--- compilation/static-lib.mk
+++ compilation/static-lib.mk
@@ -3,10 +3,6 @@ ifeq (${MODULE_CLASS}, static_lib)
 MODULE_TARGET = $(MODULE_LIB_PATH)/${MODULE}.a
 
 %.a: ${MODULE_OUTPUT_OBJS}
-	echo ${MODULE_OUTPUT_OBJS}
-	echo $(AR)
-	echo $@
-	echo $^
 	$(AR) rcs $@ $^
 
 endif
